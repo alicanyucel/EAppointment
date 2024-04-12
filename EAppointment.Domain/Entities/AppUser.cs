48a00599cf62573f4d695479df197c8538f31c52
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-04-12 15:36:54 +0300

    Entityler ekleniyor...

--- EAppointment.Domain/Entities/AppUser.cs
+++ EAppointment.Domain/Entities/AppUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace EAppointment.Domain.Entities
 {
-   public sealed class AppUser
+   public sealed class AppUser:IdentityUser<Guid>
     {
+        public string? FirstName { get; set; }=string.Empty;
+        public string? LastName { get; set;}=string.Empty;
+        public string FullName=>string.Join(" ",FirstName, LastName);
+            
+        
     }
 }
---
+++ EAppointment.Domain/Entities/Patient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAppointment.Domain.Entities
+{
+    public sealed class Patient
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string IdentityNumber { get; set; }=string.Empty;
+        public Guid Id { get; set; }
+        public string City {  get; set; } = string.Empty;
+        public string Town { get; set; } = string.Empty;
+        public string FullAdress { get; set; } = string.Empty;
+        public Patient()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
