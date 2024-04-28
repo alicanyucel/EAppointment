7cff8d5689cebafb1ea6c935be2ffc9114e9bc33
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-04-28 17:50:39 +0300

    eski kütüphane hataları çözüldü.

--- EAppointment.WebApi/Program.cs
+++ EAppointment.WebApi/Program.cs
@@ -2,15 +2,13 @@ using EAppointment.Application;
 using EAppointment.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
