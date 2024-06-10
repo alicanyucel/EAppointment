42ab970e7e1e28e926528a567c3cf335b905845c
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-06-10 18:55:18 +0300

    mola

---
+++ EAppointment.Application/Features/Patients/GetAllPatient/GetAllPatientQuery.cs
@@ -0,0 +1,14 @@
+﻿using EAppointment.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace EAppointment.Application.Features.Patients.GetAllPatient;
+public sealed record GetAllPatientQuery():IRequest<Result<List<Patient>>>;
+
+  
---
+++ EAppointment.Application/Features/Patients/GetAllPatient/GetAllPatientQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using EAppointment.Domain.Entities;
+using EAppointment.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace EAppointment.Application.Features.Patients.GetAllPatient;
+
+internal sealed class GetAllPatientQueryHandler(IPatientRepository patientRepository) : IRequestHandler<GetAllPatientQuery, Result<List<Patient>>>
+{
+    public async Task<Result<List<Patient>>> Handle(GetAllPatientQuery request, CancellationToken cancellationToken)
+    {
+        List<Patient> patients = await patientRepository.GetAll().OrderBy(p => p.FirstName).ToListAsync(cancellationToken);
+        return patients;
+    }
+}
+
+
